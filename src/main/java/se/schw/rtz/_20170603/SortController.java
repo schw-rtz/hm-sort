d819b4e9cd859501be6269acdefa6b6f5482cb30
Daniel Schwartz <schw@rtz.se>
2017-06-04 21:16:07 +0200

    Och nu sparas gamla sökningar i en fil...
    Tänk, man måste läsa hela instruktionstexten.

--- src/main/java/se/schw/rtz/_20170603/SortController.java
+++ src/main/java/se/schw/rtz/_20170603/SortController.java
@@ -1,14 +1,30 @@
 package se.schw.rtz._20170603;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 public class SortController {
+
+	private static final Logger LOGGER = Logger.getLogger(SortController.class);
+	private ObjectMapper jsonMapper;
+	private static final Object SEMAPHORE = new Object();
 
 	@RequestMapping(value = "/sort", method = RequestMethod.POST)
 	public SortResult<Double> sort(@RequestBody Double[] numbers) {
@@ -32,7 +48,11 @@ public class SortController {
 			}
 		}
 
-		return builder.timekeeperEnd().sortedList(numbers).build();
+		SortResult<Double> result = builder.timekeeperEnd().sortedList(numbers).build();
+
+		storeResult(result);
+
+		return result;
 	}
 
 	public static <T extends Comparable<T>> boolean isSorted(T[] numbers) {
@@ -53,4 +73,64 @@ public class SortController {
 		numbers[index2] = v1;
 	}
 
+	@RequestMapping(value = "/oldsort", method = RequestMethod.GET)
+	public List<SortResult<Double>> getOldSorts() {
+		synchronized (SEMAPHORE) {
+
+			Path jsonFile = getStorageFile();
+
+			try {
+				if (!Files.exists(jsonFile)) {
+					Files.write(jsonFile, getJsonMapper().writeValueAsBytes(Arrays.asList()));
+					return Arrays.asList();
+				}
+
+				byte[] byteData = Files.readAllBytes(jsonFile);
+
+				return getJsonMapper().readValue(byteData, new TypeReference<List<SortResult<Double>>>() {
+				});
+			} catch (IOException e) {
+				LOGGER.warn("could not get old data... returning empty list.");
+				return Arrays.asList();
+			}
+		}
+	}
+
+	private ObjectMapper getJsonMapper() {
+		if (jsonMapper == null) {
+			jsonMapper = new ObjectMapper();
+		}
+		return jsonMapper;
+	}
+
+	private Path getStorageFile() {
+		String homeDir = System.getProperty("user.dir");
+		String fileName = homeDir + "/hm-sort.json";
+		File jsonFile = new File(fileName);
+		return jsonFile.toPath();
+	}
+
+	private void storeResult(SortResult<Double> result) {
+		synchronized (SEMAPHORE) {
+			List<SortResult<Double>> oldSorts = new ArrayList<>(getOldSorts());
+			oldSorts.add(result);
+
+			Path jsonFile = getStorageFile();
+
+			try {
+				Files.write(jsonFile, getJsonMapper().writeValueAsBytes(oldSorts));
+			} catch (JsonProcessingException e) {
+				String message = String.format("could not store object because %s says %s",
+						e.getClass().getSimpleName(), e.getMessage());
+				LOGGER.fatal(message, e);
+				throw new RuntimeException(message, e);
+			} catch (IOException e) {
+				String message = String.format("could not store object because %s says %s",
+						e.getClass().getSimpleName(), e.getMessage());
+				LOGGER.fatal(message, e);
+				throw new RuntimeException(message, e);
+			}
+		}
+
+	}
 }
--- src/main/resources/web/sort.js
+++ src/main/resources/web/sort.js
@@ -1,11 +1,11 @@
 var sortController = (function sortController() {
-
+	
 	var resultCount = 0;
 
 	return {
 
 		callSort : function(toBeSorted) {
-			
+
 			var settings = {
 				"async" : false,
 				"crossDomain" : true,
@@ -30,11 +30,14 @@ var sortController = (function sortController() {
 
 		prepareDiv : function(divCopy, sortResult) {
 			resultCount = resultCount + 1;
-						
+
 			divCopy.attr('id', 'sortResult_' + resultCount);
-			
+
 			divCopy.children('p').attr('id', 'description_' + resultCount);
-			divCopy.children('p').text('Sortering nummer ' + resultCount + ': ' + sortResult.swaps + ' byten på ' + sortResult.elapsedTime + ' millisekunder.');
+			divCopy.children('p').text(
+					'Sortering nummer ' + resultCount + ': ' + sortResult.swaps
+							+ ' byten på ' + sortResult.elapsedTime
+							+ ' millisekunder.');
 
 			var row = divCopy.children('.row');
 			var unsortedDiv = row.children('#unsorted_0');
@@ -43,16 +46,36 @@ var sortController = (function sortController() {
 			var sortedDiv = row.children('#sorted_0');
 			sortedDiv.attr('id', 'sorted_' + resultCount);
 			sortedDiv.val(sortResult.sortedList.join());
-					
+
 			var removeButton = divCopy.children('#button_remove_0');
 			removeButton.attr('id', 'button_remove_' + resultCount);
-			removeButton.click(function () {
+			removeButton.click(function() {
 				divCopy.slideUp();
 			});
-			
+
 			return divCopy;
+		},
+
+		loadPreviousSort : function() {
+			var settings = {
+				"async" : false,
+				"crossDomain" : true,
+				"url" : "/oldsort",
+				"method" : "GET",
+				"headers" : {
+					"content-type" : "application/json",
+					"cache-control" : "no-cache",
+				}		
+			}
+
+			var result;
+			$.ajax(settings).done(function(response) {
+				console.log(response);
+				result = response;
+			});
+
+			return result;
 		}
 
 	}
-
 })();
